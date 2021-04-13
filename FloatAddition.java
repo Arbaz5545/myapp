a09fce5dd916ec97f220399f3cefd2cbd4f7369e
Arbaz5545 <64356470+Arbaz5545@users.noreply.github.com>
2021-04-13 20:14:57 +0500

    Add files via upload

---
+++ FloatAddition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class IntegerAddition {
+    public static void main(String[] args){
+        Scanner addInput = new Scanner(System.in);
+        System.out.println("Hello");
+      //  System.out.println("Welcome");
+       // float num;
+       // num = addInput.nextInt();
+        float num = addInput.nextFloat();
+       // System.out.println(num);
+        float num2 = addInput.nextFloat();
+        float ans = num + num2;
+        System.out.println(ans);
+
+    }
+}
