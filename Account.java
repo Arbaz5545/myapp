4d70ea9114ed1ab5e54fc125ccd39fba321ee671
Arbaz5545 <64356470+Arbaz5545@users.noreply.github.com>
2021-04-23 22:21:32 +0500

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,9 @@
+public class Account {
+    String firstName = "Arbaz";
+    String lastName  = "Shah";
+    String email     = "arbazshah5545@gmail.com";
+    String password  = "***************";
+    String dateOfJoin  = "Sat Apr 24 09:53:49 PKT 2021";
+
+    
+}
